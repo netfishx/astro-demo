dc0f8a253c89607de5153030c2773a35591747fa
cricket <cricket>
2025-04-07 16:47:29 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,25 @@
-import bun from '@nurodev/astro-bun'
+import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  adapter: bun(),
   output: 'server',
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
   },
+
   experimental: {
     svg: true,
     clientPrerender: true,
   },
+
   vite: {
     plugins: [tailwindcss(), tsconfigPaths()],
   },
+
+  adapter: vercel(),
 })
