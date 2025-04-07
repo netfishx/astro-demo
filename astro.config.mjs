39140f72679c18bf7e9bf235cc785b75e5faad9a
cricket <cricket>
2025-04-07 16:40:13 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import bun from '@nurodev/astro-bun'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: bun(),
+  output: 'server',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  experimental: {
+    svg: true,
+    clientPrerender: true,
+  },
+  vite: {
+    plugins: [tailwindcss(), tsconfigPaths()],
+  },
+})
